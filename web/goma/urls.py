c5330f1742d2c3a6c7d37e235ddfe7bc880d58d6
Chris Wu <chris@stripe.com>
2014-01-27 01:39:40 -0800

    Maybe a home page

--- web/goma/urls.py
+++ web/goma/urls.py
@@ -1,12 +1,9 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.conf.urls.static import static
 
-from django.contrib import admin
-
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^', 'goma.views.home', name='home'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
