3f502fc9eca6870c11ed30e6d9facdebd9c3bee4
ian <github@iantao.com>
2014-01-28 13:05:19 -0500

    separate landing and console pages

--- web/pages/urls.py
+++ web/pages/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('pages.views',
-    url(r'^$', 'home', name='home'),
-    url(r'^welcome$', TemplateView.as_view(template_name='pages/welcome.html'), name='welcome'),
+    # Landing pages
+    url(r'^welcome$', TemplateView.as_view(template_name='pages/landing/welcome.html'), name='welcome'),
+
+    # Console pages
+    url(r'^$', TemplateView.as_view(template_name='pages/console/home.html'), name='home'),
 )
