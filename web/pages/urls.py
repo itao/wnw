55e0094940e973909c9b6865066663f07211faf9
ian <github@iantao.com>
2014-01-28 12:53:11 -0500

    load initial landing page

--- web/pages/urls.py
+++ web/pages/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('pages.views',
     url(r'^$', 'home', name='home'),
+    url(r'^welcome$', TemplateView.as_view(template_name='pages/welcome.html'), name='welcome'),
 )
