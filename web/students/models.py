7169a114993c4e6e75e800aa5c7cb161b22db4da
ian <github@iantao.com>
2014-01-30 18:44:06 -0500

    create student model

--- web/students/models.py
+++ web/students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+from klasses.models import Klass
+
+class Student(models.Model):
+    account = models.ForeignKey(User)
+    picture = models.ImageField(null=True, blank=True, upload_to="attachments/students/pictures/")
+    klasses = models.ManyToManyField(Klass, blank=True, null=True)
+
+    def name(self):
+        return self.account.get_full_name()
