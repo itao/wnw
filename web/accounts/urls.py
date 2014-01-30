71bd0db85bc3f05b55c7074488a5324111f50879
ian <github@iantao.com>
2014-01-30 14:56:34 -0500

    add login/signup placeholder

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('accounts.views',
     url(r'^$', 'index', name='accounts_index'),
+    url(r'^login$', 'login', name='accounts_login'),
+    url(r'^signup$', 'signup', name='accounts_signup'),
 )
--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 
+
 def index(request):
     template = 'pages/console/accounts/index.html'
+
+    return render(
+        request,
+        template,
+        {}
+    )
+
+
+def signup(request):
+    template = 'pages/console/accounts/signup.html'
+
+    return render(
+        request,
+        template,
+        {}
+    )
+
+
+def login(request):
+    template = 'pages/console/accounts/login.html'
 
     return render(
         request,
