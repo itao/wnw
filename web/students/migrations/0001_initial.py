bd0247ddda068500fdb5fb21cb3938ab3cafae78
Chris Wu <chris@stripe.com>
2014-02-02 16:52:27 -0800

    Add dependencies for migrations

--- web/students/migrations/0001_initial.py
+++ web/students/migrations/0001_initial.py
@@ -6,6 +6,7 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (('klasses', '0001_initial'),)
 
     def forwards(self, orm):
         # Adding model 'Student'
--- web/teachers/migrations/0003_auto__add_field_teacher_school.py
+++ web/teachers/migrations/0003_auto__add_field_teacher_school.py
@@ -6,6 +6,7 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (('schools', '0001_initial'),)
 
     def forwards(self, orm):
         # Adding field 'Teacher.school'
