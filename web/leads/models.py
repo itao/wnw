1f72249f0177aac3f64206a65e6034b3268bce21
Marc Lo <lo.hungyui@gmail.com>
2014-02-04 11:42:54 -0500

    save data to database

--- web/leads/models.py
+++ web/leads/models.py
@@ -13,3 +13,10 @@ class Lead(models.Model):
    city = models.CharField(max_length=200)
    region = models.CharField(max_length=200)
    postal = models.CharField(max_length=200)
+
+def import_csv(file):
+   f = open(file, 'rb')
+   read = csv.reader(f)
+   for row in read:
+      a = Lead(name=row[0], school_type=row[1], grades=row[2], tuition=row[3], size=row[4], enrollment=row[5], phone=row[6], street=row[7], city=row[8], region=row[9], postal=row[10])
+      a.save()
