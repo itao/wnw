b6f4a8897eee4fff339c55cfa83456b4e5a5407c
Marc Lo <lo.hungyui@gmail.com>
2014-02-04 18:55:38 -0500

    even more updates

--- web/supers/views.py
+++ web/supers/views.py
@@ -10,24 +10,28 @@ from parents.models import Parent
 from teachers.models import Teacher
 
 def index(request):
-	'pages/app/supers/index.html'
 
 	b = loader.get_template('pages/app/supers/index.html')
 	h = loader.get_template('pages/app/supers/index_header.html')
+	n = loader.get_template('pages/app/supers/index_nav.html')
 	c = RequestContext(
 	    request,
 	    {
-	    	'students': Student.objects.all()
+	    	'students': Student.objects.all(),
+	    	'parents': Parent.objects.all(),
+	    	'teachers': Teacher.objects.all(),
 	    }
 	)
 	header = h.render(c)
 	body = b.render(c)
+	nav = n.render(c)
 
 	return HttpResponse(
         json.dumps({
             'title': 'Super',
             'header': header,
             'body': body,
+            'nav': nav,
         }),
         mimetype="application/json"
     )
