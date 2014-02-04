76f76248596e4d50659ad9994b6e9b9ab634ab87
Marc Lo <lo.hungyui@gmail.com>
2014-02-04 16:16:57 -0500

    added html for supers

--- web/supers/views.py
+++ web/supers/views.py
@@ -10,8 +10,17 @@ from parents.models import Parent
 from teachers.models import Teacher
 
 def index(request):
-	template = 'layouts/app_base.html'
+	template = 'pages/app/supers/index.html'
     
+	t = loader.get_template(template)
+	c = RequestContext(
+	    request,
+	    {
+	        'student': student
+	    }
+	)
+	html = t.render(c)
+
 	return HttpResponse(
         json.dumps({
             'title': 'Class Name - Students',
@@ -21,7 +30,7 @@ def index(request):
     )
 
 def students_stats(request):
-	template = 'layouts/app_base.html'
+	template = 'pages/app/supers/students_stats.html'
 	students = Student.objects.all()
 	names = students.name
 
@@ -36,31 +45,49 @@ def students_stats(request):
 
 	return HttpResponse(
 	    json.dumps({
-	        'title': student.name(),
+	        'title': 'Students Statistics',
 	        'html': html,
 	    }),
 	    mimetype="application/json"
 	)
 
 def parents_stats(request):
-	template = 'layouts/app_base.html'
+	template = 'pages/app/supers/parents_stats.html'
 	parents = Parent.objects.all()
+
+	t = loader.get_template(template)
+	c = RequestContext(
+	    request,
+	    {
+	        'parent': parent
+	    }
+	)
+	html = t.render(c)
 
 	return HttpResponse(
 	    json.dumps({
-	        'title': student.name(),
+	        'title': 'Parents Statistics',
 	        'html': html,
 	    }),
 	    mimetype="application/json"
 	)
 
 def teachers_stats(request):
-	template = 'layouts/app_base.html'
+	template = 'pages/app/supers/teachers_stats.html'
 	teachers = Teacher.objects.all()
+
+	t = loader.get_template(template)
+	c = RequestContext(
+	    request,
+	    {
+	        'teacher': teacher
+	    }
+	)
+	html = t.render(c)
 
 	return HttpResponse(
 	    json.dumps({
-	        'title': student.name(),
+	        'title': 'Teachers Statistics',
 	        'html': html,
 	    }),
 	    mimetype="application/json"
