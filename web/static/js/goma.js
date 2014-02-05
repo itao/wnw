5f492b8bfce9049c00b3b88de360909e88eb0884
itao <github@iantao.me>
2014-02-05 00:27:34 -0500

    Make nav item active

--- web/static/js/goma.js
+++ web/static/js/goma.js
@@ -7,10 +7,13 @@ function updateAppBody(elem) {
         type: "GET",
         url: $(elem).attr('data-url'),
         success: function(data) {
+            // Set nav item as the only active
             $(elem).siblings().removeClass('active')
             $(elem).addClass('active');
+
+            // Update content
             if (data.title) {
-                $('#content-title').html(data.title);
+                updateContentTitle(data.title);
             }
             if (data.header) {
                 $('#header-actions').html(data.header);
