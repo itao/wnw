96811b24b5750b98baa1662cb01519305ddbe9c4
Marc Lo <lo.hungyui@gmail.com>
2014-02-05 11:24:02 -0500

    modified teacher models

--- web/teachers/models.py
+++ web/teachers/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 from accounts.models import User
 from schools.models import School
+# from klasses.models import Klass
+from notes.models import Note
 
 class Teacher(models.Model):
     account = models.ForeignKey(User)
     picture = models.ImageField(null=True, blank=True, upload_to="teachers/pictures")
     school = models.ForeignKey(School, null=True, blank=True)
+    # klasses = models.ManyToManyField(Klass, blank=True, null=True)
+    notes = models.ForeignKey(Note, blank=True, null=True)
 
     def name(self):
         return self.account.get_full_name()
