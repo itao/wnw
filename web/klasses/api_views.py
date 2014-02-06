bb1580401523189002617614b5b8017e4505f133
Yunyi Tang <yytang9862@gmail.com>
2014-02-06 05:21:30 -0500

    additional api for new structure, can call all students from a class

--- web/klasses/api_views.py
+++ web/klasses/api_views.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import link
+from rest_framework.response import Response
 
 from .models import Klass
 from .serializers import KlassSerializer
+from students.serializers import StudentSerializer
 
 class KlassViewSet(viewsets.ModelViewSet):
     model = Klass
     serializer_class = KlassSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    @link(renderer_classes=[renderers.StaticHTMLRenderer])
+    def students(self, request, pk):
+    	klass = Klass.objects.get(pk=pk)
+    	students = klass.students.all()
+    	student_serializer = StudentSerializer(students, many=True)
+    	return Response(student_serializer.data)
