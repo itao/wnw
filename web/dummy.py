ab91af592471b3ea4922832ac453a0d55adf353e
itao <github@iantao.me>
2014-02-07 17:38:15 -0500

    update dummy script for new schema
    and add prompt before clean()

--- web/dummy.py
+++ web/dummy.py
@@ -17,62 +17,70 @@ def clean():
 
 
 def init():
-    # Wipe db
-    clean()
+    print 'Existing database will be wiped, continue? (y/n)'
+    choice = raw_input().strip()
+    if choice == 'y':
 
-    # Create users
-    u = User(first_name='Demo', last_name='Teacher', email='demo@sesame.io')
-    u.save()
-    u.set_password('sesame')
-    u.save()
+        print 'Initializing...'
 
-    # Create teachers
-    t = Teacher(account=u)
-    t.save()
+        # Wipe db
+        clean()
 
-    # Create classes
-    for i in range(3):
-        index = str(i+1)
-        name = 'Class #' + index
-        code = 'KLS' + index
-        start = end = datetime.now().date()
-        colour = "#" + index*6
+        # Common password
+        pw = 'sesame'
 
-        k = Klass(teacher=t, name=name, code=code, start=start, end=end, colour=colour)
-        k.save()
+        # Create teachers
+        t = Teacher(first_name='Sesame', last_name='Teacher', email='teacher@sesame.io')
+        t.save()
+        t.set_password(pw)
+        t.save()
 
-    # Create students
-    students = [
-        {
-            'first_name': 'Alton',
-            'last_name': 'Lau',
-            'email': 'alton@lau.com'
-        }, {
-            'first_name': 'Marc',
-            'last_name': 'Lo',
-            'email': 'marc@lo.com'
-        }, {
-            'first_name': 'Madigan',
-            'last_name': 'Kim',
-            'email': 'madigan@kim.com'
-        }
-    ]
-    classes = Klass.objects.all()
-    for i in range(3):
-        s = students[i]
-        u = User(first_name=s['first_name'], last_name=s['last_name'], email=s['email'])
-        u.save()
-        s = Student(account=u)
-        s.save()
-        s.klasses = classes[:i+1]
+        # Create classes
+        for i in range(3):
+            index = str(i+1)
+            name = 'Class #' + index
+            code = 'KLS' + index
+            start = end = datetime.now().date()
+            colour = "#" + index*6
+
+            k = Klass(teacher=t, name=name, code=code, start=start, end=end, colour=colour)
+            k.save()
+
+        # Create students
+        students = [
+            {
+                'first_name': 'Alton',
+                'last_name': 'Lau',
+                'email': 'alton@lau.com'
+            }, {
+                'first_name': 'Marc',
+                'last_name': 'Lo',
+                'email': 'marc@lo.com'
+            }, {
+                'first_name': 'Madigan',
+                'last_name': 'Kim',
+                'email': 'madigan@kim.com'
+            }
+        ]
+        classes = Klass.objects.all()
+        for i in range(3):
+            s = students[i]
+            s = Student(first_name=s['first_name'], last_name=s['last_name'], email=s['email'])
+            s.save()
+            s.set_password(pw)
+            s.save()
+            s.klasses = classes[:i+1]
 
 
-    k = Klass.objects.all()[0]
-    s = Student.objects.all()
-    # Create notes
-    for i in range(3):
-        index = str(i+1)
-        detail = 'This is note #' + index
-        n = Note(klass=k, detail=detail)
-        n.save()
-        n.students = s[:i+1]
+        k = Klass.objects.all()[0]
+        s = Student.objects.all()
+        # Create notes
+        for i in range(3):
+            index = str(i+1)
+            detail = 'This is note #' + index
+            n = Note(klass=k, detail=detail)
+            n.save()
+            n.students = s[:i+1]
+        print 'All done, goodbye!'
+    else:
+        print 'Fine, I see how it is.'
