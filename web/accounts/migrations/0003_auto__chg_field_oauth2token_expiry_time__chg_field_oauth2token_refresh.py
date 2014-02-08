7a3e47246e8c3589302accb439faad39830e84b0
Jerry Sun <j.x.y.sun@gmail.com>
2014-02-08 00:20:38 -0800

    fixed up model

---
+++ web/accounts/migrations/0003_auto__chg_field_oauth2token_expiry_time__chg_field_oauth2token_refresh.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Oauth2Token.expiry_time'
+        db.alter_column(u'accounts_oauth2token', 'expiry_time', self.gf('django.db.models.fields.DateTimeField')(null=True))
+
+        # Changing field 'Oauth2Token.refresh_token'
+        db.alter_column(u'accounts_oauth2token', 'refresh_token', self.gf('django.db.models.fields.CharField')(max_length=1024))
+
+    def backwards(self, orm):
+
+        # Changing field 'Oauth2Token.expiry_time'
+        db.alter_column(u'accounts_oauth2token', 'expiry_time', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 2, 8, 0, 0)))
+
+        # Changing field 'Oauth2Token.refresh_token'
+        db.alter_column(u'accounts_oauth2token', 'refresh_token', self.gf('django.db.models.fields.DateTimeField')(max_length=1024))
+
+    models = {
+        u'accounts.oauth2token': {
+            'Meta': {'object_name': 'Oauth2Token'},
+            'User': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['accounts.User']"}),
+            'access_token': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'expiry_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id_token': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'refresh_token': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'accounts.user': {
+            'Meta': {'object_name': 'User'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['accounts']
